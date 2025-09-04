dc276d581475ff6a327a816320d6da0ade4ca40f
Cesar Cruz <cesarcruzcanul210203@gmail.com>
2025-09-04 17:06:19 -0600

    Age
    se agregó tanto en el modelo como en la migración el campo AGE

--- database/migrations/2025_09_04_000001_create_patients_table.php
+++ database/migrations/2025_09_04_000001_create_patients_table.php
@@ -13,6 +13,7 @@ class CreatePatientsTable extends Migration
             $table->string('medical_record_number', 20)->unique();
             $table->string('full_name', 255);
             $table->date('date_of_birth');
+            $table->integer('age');
             $table->string('sex', 50);
             $table->string('patient_type', 100);
             $table->string('service', 150);
